dd0b91631a28b2b8b0022b278b0f132d9b34adcc
Julio Jiménez <juliojimr@gmail.com>
2014-04-14 21:22:16 +0200

    Commit final, funcionando

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,28 +15,25 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
+  private static final long serialVersionUID = 1L;
+  private static String DELETE_JSP = "/Delete.jsp";
+  private static String EDIT_JSP = "/Edit.jsp";
+  private static String SHOWALL_JSP = "/ShowAll.jsp";
+
+  
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String forward="";
+    // Get a map of the request parameters
+    @SuppressWarnings({ "rawtypes" })
+    Map parameters = request.getParameterMap();
+    if (parameters.containsKey("delete")){
+      forward = DELETE_JSP;
+    } else if (parameters.containsKey("edit")){
+      forward = EDIT_JSP;
+    } else {
+      forward = SHOWALL_JSP;
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
-}
+    RequestDispatcher view = request.getRequestDispatcher(forward);
+    view.forward(request, response);
+  }
+} 
